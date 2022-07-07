9e90067aef968db1bb769fe55b19dc476c3e0cb1
harikiran19 <96737891+harikiran19@users.noreply.github.com>
2022-07-07 14:36:23 +0530

    n=int(input()) arr=list(map(int,input().split())) sum,count=0,0 for i in range (0,n):     sum+=arr[i] av=sum//n for i in range(0,n):     if arr[i]>=av:         count+=1 print(count)

---
+++ elements_greater_than_average.py
@@ -0,0 +1,10 @@
+n=int(input())
+arr=list(map(int,input().split()))
+sum,count=0,0
+for i in range (0,n):
+    sum+=arr[i]
+av=sum//n
+for i in range(0,n):
+    if arr[i]>=av:
+        count+=1
+print(count)
