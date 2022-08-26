5ceb18429109ba842c8aa6d8040a3229929ebb59
harikiran19 <96737891+harikiran19@users.noreply.github.com>
2022-08-26 10:35:08 +0530

    def rev(n):     r=0     temp=n     while (n>0):         re=n%10         r=r*10+re         n=n//10     if (r==temp):         return True c=0 n=int(input()) lt=list(map(int,input().split())) for i in lt:     if rev(i):         c+=1 print(c)

--- Count_palindromes.py
+++ Count_palindromes.py
@@ -1,12 +1,16 @@
-def prob(n):
-    st=str(n)
-    r=st[::-1]
-    if st==r:
+def rev(n):
+    r=0
+    temp=n
+    while (n>0):
+        re=n%10
+        r=r*10+re
+        n=n//10
+    if (r==temp):
         return True
 c=0
 n=int(input())
 lt=list(map(int,input().split()))
 for i in lt:
-    if prob(i):
+    if rev(i):
         c+=1
 print(c)
