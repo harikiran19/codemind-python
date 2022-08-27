ea94ee761d7d6648bcc26ba034407d0c95df0c8b
harikiran19 <96737891+harikiran19@users.noreply.github.com>
2022-08-27 11:02:12 +0530

    x,y = map(int,input().split()) l = list(map(str,input().split())) b=[] for i in l:     if int(i)<0:         i = int(i)*-1         b.append(len(str(i)))     else:         b.append(len(i)) print(b.count(y))

---
+++ K_digit_length_numbers_in_a_array.py
@@ -0,0 +1,10 @@
+x,y = map(int,input().split())
+l = list(map(str,input().split()))
+b=[]
+for i in l:
+    if int(i)<0:
+        i = int(i)*-1
+        b.append(len(str(i)))
+    else:
+        b.append(len(i))
+print(b.count(y))
