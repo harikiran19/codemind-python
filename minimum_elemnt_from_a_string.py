880c438986044eddf3b11e9fe02aa05f443eeccb
harikiran19 <96737891+harikiran19@users.noreply.github.com>
2022-09-23 18:49:19 +0530

    x = list(map(str,input().split())) x = x[len(x)-1] n = min(x) m=n if n.isupper():     if n.lower() in x:         print(n.lower())     else:         print(n) else:     print(n)

--- minimum_elemnt_from_a_string.py
+++ minimum_elemnt_from_a_string.py
@@ -1,9 +1,11 @@
-s=list(map(str,input().split()))
-a=len(s)
-s=s[a-1]
-n=min(s)
-m=n.lower()
-if s.count(m)!=0:
-    print(m)
+x = list(map(str,input().split()))
+x = x[len(x)-1]
+n = min(x)
+m=n
+if n.isupper():
+    if n.lower() in x:
+        print(n.lower())
+    else:
+        print(n)
 else:
     print(n)
